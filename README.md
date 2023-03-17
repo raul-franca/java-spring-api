a89f67a480333641ef82cb477e49e8d85f7a969c
Raul França <raulmfranca@gmail.com>
2023-03-17 01:32:05 -0300

    Security and JWT

--- README.md
+++ README.md
@@ -1,15 +1,31 @@
 
 
 
-_API REST_  utilizando  [_Spring Boot_](https://spring.io/projects/spring-boot)
-O projeto é um CRUD de medicos, pacinetes e marcacao de consultas
-[_Spring Initializr_](https://start.spring.io)_  para fazer a construção do projeto.
+# _API REST_  utilizando  [_Spring Boot_](https://spring.io/projects/spring-boot)
+O projeto é um App para uma clinica medica, sistema realiza CRUD de medicos, pacinetes e marcação de consultas, a autenticação dos usuarios com token JWT.
 
-Maven para a gestão de dependências e um task runner.
+[_Spring Initializr_](https://start.spring.io)_  para fazer a construção do projeto -   **Java**: 17.
 
-módulos, como Web, Validação e  _Spring Data JPA_.
-E outras bibliotecas, como o driver do  _MySQL, o Flyway e o Lombok_.
+### Maven para a gestão de dependências e _build_.
+####  Dependências
+- Spring Boot DevTools
+- Spring Data JPA
+- Spring Web
+- Spring Security
+- Lombok
+- Validation
+
+E outras bibliotecas, como o driver do  _MySQL, Flyway e token [java-jwt](https://github.com/auth0/java-jwt) .
 
 Flyway: para fazer as migrations para ter controle do histórico de evolução do banco de dados.
 
-Lombok: Framework para Java que permite escrever código eliminando a verbosidade, o que permite ganhar tempo de desenvolvimento 
+Lombok é Framework, para gerar códigos, como esses códigos verbosos do Java, de `getter` e `setter`, baseado em anotações. serve para deixar  o código mais simples e menos verboso.
+
+### Tudo que Ja foi implementado
+-   Desenvolvimento de uma API Rest
+-   CRUD (Create, Read, Update e Delete)
+-   Validações
+-   Paginação e ordenação
+-   Boas práticas REST
+-   Tratamento de erros
+-   Controle de acesso com JWT
--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -1,25 +1,46 @@
 package med.voll.api.infra.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfigurations {
-//    Estamos desabilitando o tratamento contra-ataque CSRF e, na sequência,
-//    desabilitando o processo de autenticação padrão do Spring. Isso porque estamos usando uma API Rest,
-//    e queremos que seja stateless.
+
+    @Autowired
+    private SecurityFilter securityFilter;
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http.csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().build();
+                .and().authorizeRequests()
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and().addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
 }
